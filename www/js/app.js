618789be21a167b93a38c7e7e43083c6cde3bf1f
Ajay <ajay@techjini.com>
2016-12-19 17:45:30 +0530

    minor changes

--- www/js/app.js
+++ www/js/app.js
@@ -7,7 +7,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, MFPInit) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -20,6 +20,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    MFPInit.then(function(){WL.Logger.ctx({pkg: 'io.ionic'}).debug('mfp and ionic are ready, safe to use WL.* APIs');});
   });
 })
 
@@ -82,4 +83,48 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
 
+})
+.factory('MFPInit', function($q){
+  /* Setup a Promise to allow code to run in other places anytime after MFP CLient SDK is ready
+     Example: MFPClientPromise.then(function(){alert('mfp is ready, go ahead and use WL.* APIs')});
+  */
+  return window.MFPClientDefer.promise;
 });
+
+window.Messages = {
+  // Add here your messages for the default language.
+  // Generate a similar file with a language suffix containing the translated messages.
+  // key1 : message1,
+};
+
+window.wlInitOptions = {
+  // Options to initialize with the WL.Client object.
+  // For initialization options please refer to IBM MobileFirst Platform Foundation Knowledge Center.
+};
+
+window.MFPClientDefer = angular.injector(['ng']).get('$q').defer();;
+window.wlCommonInit = window.MFPClientDefer.resolve;
+window.MFPClientDefer.promise.then(function wlCommonInit(){
+  // Common initialization code goes here or use the angular service MFPClientPromise
+  
+  console.log('MobileFirst Client SDK Initilized');
+  mfpMagicPreviewSetup();
+ });
+ 
+function notificationReceived(message) {
+    obj = JSON.parse(message.payload);
+    alert("Push received",message.payload);
+}; 
+
+function mfpMagicPreviewSetup(){
+  var platform;
+  //nothing to see here :-), just some magic to make ionic work with mfp preview, similar to ionic serve --lab
+  if(WL.StaticAppProps.ENVIRONMENT === 'preview'){
+    //running mfp preview (MBS or browser)
+    platform = WL.StaticAppProps.PREVIEW_ENVIRONMENT === 'android' ? 'android' : 'ios';
+    if(location.href.indexOf('?ionicplatform='+platform) < 0){
+      location.replace(location.pathname+'?ionicplatform='+platform);
+    }
+  } 
+}
+
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,46 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, MFPInit) {
+  MFPInit.then(function() {
+      WL.App.getServerUrl(function(url) {
+        $scope.serverurl = url;
+      });
+      if(typeof MFPPush !== 'undefined'){
+        MFPPush.initialize(
+          function(successResponse) {
+            console.log("Initialized",successResponse);
+            WL.Logger.debug("Successfully intialized");
+            MFPPush.registerNotificationsCallback(notificationReceived);
+        }, function(failureResponse) {
+            alert("Failed to initialize");
+        });
+      }
+
+    });
+  $scope.$on('$ionicView.enter', function() {
+      MFPInit.then(function() { WL.Analytics.log({ AppView: 'MFPF' }, "visit mfpf view"); console.log("mfpf view enter") });
+    });
+
+    function greetAdapter() {
+      var resourceRequest = new WLResourceRequest(
+        "/adapters/javaAdapter/resource/greet",
+        WLResourceRequest.GET
+      );
+      resourceRequest.setQueryParameter("name", "Ajay");
+
+      resourceRequest.send().then(
+        function(response) {
+          WL.Logger.debug("Adapter response: " + response.responseText);
+          document.getElementById("resultGreet").innerHTML = "Adapter said: " + response.responseText;
+        },
+        function(response) {
+          WL.Logger.debug("Failed to call adapter: " + JSON.stringify(response));
+          document.getElementById("resultGreet").innerHTML = "Failed to call adapter.";
+        });
+    }
+    $scope.greetAdapter = greetAdapter;
+
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
@@ -25,4 +65,84 @@ angular.module('starter.controllers', [])
   $scope.settings = {
     enableFriends: true
   };
-});
+})
+
+  .run(function($rootScope, $ionicModal, $timeout, MFPInit) {
+    try {
+      console.log(wl_DirectUpdateChallengeHandler);
+    } catch(err) {
+      console.log(err);
+    }
+    //MobileFirst Authentication setup
+    var securityCheckName = 'UserLogin';
+
+    MFPInit.then(function() {
+      LoginChallenge = new WL.Client.createSecurityCheckChallengeHandler(securityCheckName);
+
+      LoginChallenge.securityCheckName = securityCheckName;
+
+      LoginChallenge.handleChallenge = function(response) {
+        $rootScope.login();
+      };
+
+      LoginChallenge.processSuccess = function(data) {
+        WL.Logger.debug("processSuccess");
+        console.log("LoginChallenge.processSuccess");
+      };
+
+      LoginChallenge.handleFailure = function(error) {
+        console.log("LoginChallenge.handleFailure");
+      };
+
+
+      $rootScope.doLogin = function() {
+        console.log('Submitting LoginData', $rootScope.loginData.username);
+        $rootScope.closeLogin();
+        /*
+        $timeout(function() {
+          LoginChallenge.submitChallengeAnswer({
+            'username': $rootScope.loginData.username,
+            'password': $rootScope.loginData.password
+          });
+        }, 3000, false, [Pass]);
+        */
+        LoginChallenge.submitChallengeAnswer({
+          'username': $rootScope.loginData.username,
+          'password': $rootScope.loginData.password
+        });
+
+      };
+
+
+      $rootScope.doLogout = function() {
+        WLAuthorizationManager.logout(securityCheckName).then(
+          function() {
+            WL.Logger.debug("logout onSuccess");
+            location.reload();
+          },
+          function(response) {
+            WL.Logger.debug("logout onFailure: " + JSON.stringify(response));
+          });
+      }
+    });
+
+
+    $rootScope.loginData = {};
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $rootScope
+    }).then(function(modal) {
+      $rootScope.modal = modal;
+    });
+
+    // Triggered in the login modal to close it
+    $rootScope.closeLogin = function() {
+      $rootScope.modal.hide();
+    };
+
+    // Open the login modal
+    $rootScope.login = function() {
+      $rootScope.modal.show();
+    };
+
+  })
